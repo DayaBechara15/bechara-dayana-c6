897d6caf17a33c4337f33046e9257caeb604ab76
Dayanna Bechara <dayanabechara@hotmail.com>
2023-03-07 22:44:13 -0500

    primera parte parcial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./components/Card";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <Form />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
