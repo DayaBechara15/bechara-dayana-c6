d4de6a659618a464ddcdb4216bdfc68f12e65cb9
Dayanna Bechara <dayanabechara@hotmail.com>
2023-03-09 22:04:28 -0500

    parcialterminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,72 @@
 import "./App.css";
+import React, {useState} from "react";
 import Card from "./components/Card";
-import Form from "./components/Form";
 
 function App() {
+  const [auto, setAuto] = useState({
+    modelo: "",
+    color: "",
+    marca: "",
+  });
+  const [error, setError] = useState({error: false, mensaje: ""});
+  const [isUploaded, setIsUploaded] = useState(false);
+
+  const handleChange = (e, input) => {
+    setAuto({...auto, [input]: e.target.value.trim()}); //se usa trim para eliminar espacios en blanco
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(auto.modelo === "" || auto.color === "" || auto.marca === ""){
+      setError({error: true, mensaje: "Todos los campos son obligatorios"});
+      return;
+    }else if(auto.modelo.length < 3){
+      setError({error: true, mensaje: "El modelo debe tener al menos 3 caracteres"});
+      return;
+    }else if(auto.marca.length < 6){
+      setError({error: true, mensaje: "La marca debe tener al menos 6 caracteres"});
+      return;
+    }else{
+      setError({error: false, mensaje: ""});
+      setIsUploaded(true);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <Form />
-      <Card />
+      <div className="form-box">
+        <h1>Carga de Autos</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <label>Modelo</label>
+          <input
+            type="text"
+            name="modelo"
+            onChange={(e) => handleChange(e, "modelo")}
+          />
+          <label>Marca</label>
+          <input
+            type="text"
+            name="marca"
+            onChange={(e) => handleChange(e, "marca")}
+          />
+          <label>Color</label>
+          <input
+            type="text"
+            name="color"
+            onChange={(e) => handleChange(e, "color")}
+          />
+          <button type="submit">Ingresar Auto</button>
+        </form>
+         {error.error && (
+          <p className="error">
+            Por favor chequea que la información sea correcta: <br/>
+            {error.mensaje}
+          </p>
+        )}
+        {isUploaded && (
+          <Card auto={auto}></Card>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Card() {
-  return <>Tarjeta</>;
+function Card({auto}) {
+  const {modelo, marca, color} = auto;
+  return (
+    <div className="card">
+      <h1>Auto registrado:</h1>
+      <p>Modelo: {modelo}</p>
+      <p>Marca: {marca}</p>
+      <p>Color: {color}</p>
+    </div>
+  );
 }
 
 export default Card;
